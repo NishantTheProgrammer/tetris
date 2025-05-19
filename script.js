cd80ba624a3c6ec2a0f158836b55cd5cbd0b2798
NishantTheProgrammer <knishant113@gmail.com>
2025-05-20 00:21:49 +0530

    grid setup

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 console.log('hello')
+
+const M = 20;
+const N = 10;
+let grid;
+const intializeGrid = () => {
+    grid = new Array(N).fill(0).map(() => {
+        return new Array(M).fill(0);
+    })
+}
+
+const render = () => {
+    console.log(grid);
+
+    const innerHTML = grid.map(row => {
+        return `
+            <div class="row">
+            ${
+                row.map(col => {
+                    return `<div class="box">${col}</div>`
+                }).join('')
+            }
+            </div>
+        `;
+    }).join('');
+
+    document.getElementById('root').innerHTML = innerHTML;
+}
+
+
+
+const startGame = () => {
+    intializeGrid();
+    render();
+}
+
+
+startGame();
